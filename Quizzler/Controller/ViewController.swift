39827fdf87761298402d1714f49c2145ad5da75e
Li Yong Teng <leonteng91@gmail.com>
2022-06-24 09:56:10 +0200

    Initial Commit
    Initial Commit

---
+++ Quizzler/Controller/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var quizQNA = QuizQNA()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        sender.backgroundColor = quizQNA.checkAnswer(userAnswer) ? UIColor.green : UIColor.red
+        quizQNA.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI(){
+        questionLabel.text = quizQNA.getQuestionText()
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        scoreLabel.text = "Score: \(quizQNA.getScore())"
+        progressBar.progress = quizQNA.getProgress()
+    }
+    
+}
+
